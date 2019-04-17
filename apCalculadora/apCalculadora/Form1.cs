3e48d54f5970c30219a7995c10ec6ade7b59f8b6
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-04-17 13:46:48 -0300

    ahh

--- apCalculadora/apCalculadora/Form1.cs
+++ apCalculadora/apCalculadora/Form1.cs
@@ -41,8 +41,49 @@ namespace apCalculadora
         }
 
         private void btnIgual_Click(object sender, EventArgs e)
-        {
+        { 
+            string texto = txtVisor.Text;
+            string[] vetor = null;
+            int cont = 0;
+
+            while (cont <= texto.Length)
+                foreach (var a in texto)
+                    vetor[cont] = a + "";
+
+            SepararExpressao(vetor);
+
             txtVisor.Clear();
+        }
+
+        public void SepararExpressao(string[] vetor)
+        {
+            Pilha pilha = null;
+
+            if (!EstaEmOrdem(vetor))
+                return;
+
+            for (int b = 0; b < vetor.Length; b++)
+                if (char.Parse(vetor[b]) >= '0' && char.Parse(vetor[b]) <= '9') //Se o caractere atual for número
+                {
+                    if (vetor[b - 1] != null && char.Parse(vetor[b - 1]) >= '0' && char.Parse(vetor[b - 1]) <= '9')
+                    {
+                        pilha.ExcluirTopo();
+                        pilha.InserirNoTopo(vetor[b - 1] + vetor[b], "número");
+                    }                      
+                    else
+                        pilha.InserirNoTopo(vetor[b], "número");
+                }
+                else
+                    if(char.Parse(vetor[b]) >= 'A' && char.Parse(vetor[b]) <= 'Z')  //Se o caractere atual for sinal
+                         MessageBox.Show("Erro");
+                else //É um sinal
+                    pilha.InserirNoTopo(vetor[b] , "sinal");
+
+        }
+
+        public bool EstaEmOrdem(string[] vetor)
+        {
+
         }
     }
 }
