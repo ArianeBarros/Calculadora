d628ea6ffd89167c75f71839e85a7ecdf9539425
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-04-24 10:45:22 -0300

    ahhhh

--- apCalculadora/apCalculadora/Expressao.cs
+++ apCalculadora/apCalculadora/Expressao.cs
@@ -14,164 +14,224 @@ namespace apCalculadora
             pilha = new PilhaHerdaLista<string>();
         }
 
-        public string ParaPosfixa(string str)
+        public string ParaPosfixa(string expressaoInteira)
+        {
+            string pos = "";
+            char ultimoCaractere = ' ';
+            int contSinal = 0;
+            int qtdCarac = 0;
+
+            while(qtdCarac != expressaoInteira.Length)
+            {
+                while (contSinal != 2)
+                {
+                    foreach (var c in expressaoInteira)
+                    {
+                        if (c == '+' || c == '-' || c == '/' || c == '*' || c == '^')
+                            contSinal++;
+                        else
+                            ultimoCaractere = c;
+
+                        qtdCarac++;
+                    }                       
+                }
+                string aux = expressaoInteira.Split(ultimoCaractere)[0];
+                pos += UmaParteParaPosfixa(aux);
+            }         
+
+            return pos;
+        }
+
+        public string UmaParteParaPosfixa(string str)
         {
             string posfixa = "";
-            string infixa = str;
             string operadorA = "";
             string operadorB = "";
             string sinal = "";
 
-            for (int i = 0; i < infixa.Length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
-                if (infixa[i] >= '0' && infixa[i] <= '9')//Se for número
+                if (str[i] >= '0' && str[i] <= '9')//Se for número
+                { 
                     if (operadorA == "")
                     {
-                        operadorA = infixa[i] + "";
+                        operadorA = str[i] + "";
                         posfixa += operadorA;
                     }
                     else
                     {
                         if (sinal == "") //não achamos um sinal ainda
                         {
-                            operadorA += infixa[i];
-                            posfixa = posfixa.Split(posfixa[i--])[0];
+                            posfixa = "";
+                            operadorA += str[i] + "";
                             posfixa += operadorA;
                         }
                         else //já achamos um sinal, portanto o próximo valor será o operador B
                         {
-                            if (operadorB == "")
+                            if(sinal == ",")
                             {
-                                operadorB = infixa[i] + "";
+                                operadorA += str[i] + "";
+                                posfixa = operadorA;
+                                sinal = "";
+                            }
+
+                            if (i < str.Length - 1)
+                            {
+                                if (operadorB == "")
+                                    operadorB = str[i] + "";
+                                else
+                                {
+                                    posfixa = operadorA;
+                                    operadorB += str[i] + "";
+                                }
                                 posfixa += operadorB;
                             }
-                            else
+                            else //a sequencia string vai acabar
                             {
-                                operadorB += infixa[i] + "";
-                                posfixa = posfixa.Split(posfixa[i--])[0];
+                                if (operadorB == "")
+                                    operadorB = str[i] + "";
+                                else
+                                {
+                                    posfixa = operadorA;
+                                    operadorB += str[i] + "";
+                                }
                                 posfixa += operadorB + sinal;
+                                operadorA = "";
+                                operadorB = "";
+                                sinal = "";
                             }
                         }
                     }
+                }
                 else
-                    if (posfixa[i] >= 'A' && posfixa[i] <= 'Z') //se for sinal
+                    if (str[i] >= 'A' && str[i] <= 'Z') //se não for sinal
                     {
-                    //ueee
-                    }
-                else
-                    if (sinal == "")
-                    {
-                        if (sinal == ",")
-                            operadorA += ",";
-                        else
-                            sinal = posfixa[i] + "";
+                        //ueee
                     }
                     else
-                    {
-                        //uee
-                    }
-                    // }
+                        if (sinal == "")
+                        {
+                            if (sinal == ",")
+                            {
+                                operadorA += ",";
+                                posfixa += ",";
+                            }                                
+                            else
+                                sinal = str[i] + "";
+                        }
+                        else //outra sequencia vai começar
+                        {
+                            if (operadorB == "")
+                                operadorB = "0";
+                                                       
+                            posfixa += operadorB + sinal;
+                            operadorA = "";
+                            operadorB = "";
+                            sinal = "";
+                        }
             }
+
             return posfixa;
         }
         public void Resolver()
         {
-            string posfixa = this.ToString();
-            string operadorA = "";
-            string operadorB = "";
-            string sinal = "";
 
-            for(int i = 0; i < posfixa.Length; i++)
-            {
-                if (posfixa[i] >= '0' && posfixa[i] <= '9')//Se for número
-                    if (operadorA == "")
-                    {
-                        operadorA = posfixa[i] + "";
-                        pilha.Empilhar(operadorA);
-                    }                        
-                    else
-                    {
-                        if (sinal == "") //não achamos um sinal ainda
-                        {
-                            operadorA += posfixa[i];
-                            pilha.Desempilhar();
-                            pilha.Empilhar(operadorA);
-                        }                           
-                        else //já achamos um sinal, portanto o próximo valor será o operador B
-                        {
-                            if (operadorB == "")
-                            {
-                                operadorB = posfixa[i] + "";
-                                pilha.Empilhar(operadorB);
-                            }                             
-                            else
-                            {
-                                operadorB += posfixa[i] + "";
-                                pilha.Desempilhar();
-                                pilha.Empilhar(operadorB);
-                                pilha.Empilhar(sinal);
+            //string posfixa = this.ToString();
+            //string operadorA = "";
+            //string operadorB = "";
+            //string sinal = "";
 
-                                sinal = pilha.Desempilhar();
-                                operadorB = pilha.Desempilhar();
-                                operadorA = pilha.Desempilhar();
+            //for(int i = 0; i < posfixa.Length; i++)
+            //{
+            //    if (posfixa[i] >= '0' && posfixa[i] <= '9')//Se for número
+            //        if (operadorA == "")
+            //        {
+            //            operadorA = posfixa[i] + "";
+            //            pilha.Empilhar(operadorA);
+            //        }                        
+            //        else
+            //        {
+            //            if (sinal == "") //não achamos um sinal ainda
+            //            {
+            //                operadorA += posfixa[i];
+            //                pilha.Desempilhar();
+            //                pilha.Empilhar(operadorA);
+            //            }                           
+            //            else //já achamos um sinal, portanto o próximo valor será o operador B
+            //            {
+            //                if (operadorB == "")
+            //                {
+            //                    operadorB = posfixa[i] + "";
+            //                    pilha.Empilhar(operadorB);
+            //                }                             
+            //                else
+            //                {
+            //                    operadorB += posfixa[i] + "";
+            //                    pilha.Desempilhar();
+            //                    pilha.Empilhar(operadorB);
+            //                    pilha.Empilhar(sinal);
 
-                                pilha.Empilhar(ResolverUmaOperacao(operadorA, operadorB, sinal));
-                            }                               
-                        }
-                    }
-                else
-                    if (!(posfixa[i] >= 'A') && !(posfixa[i] <= 'Z')) //se for sinal
-                    {
-                        if (sinal == "")
-                        {
-                            if(sinal == ",")
-                                operadorA += ",";
-                            else
-                                sinal = posfixa[i] + "";
-                        }
-                        else
-                        {
-                           //uee
-                        }                            
-                    }               
-            }
+            //                    sinal = pilha.Desempilhar();
+            //                    operadorB = pilha.Desempilhar();
+            //                    operadorA = pilha.Desempilhar();
+
+            //                    pilha.Empilhar(ResolverUmaOperacao(operadorA, operadorB, sinal));
+            //                }                               
+            //            }
+            //        }
+            //    else
+            //        if (!(posfixa[i] >= 'A') && !(posfixa[i] <= 'Z')) //se for sinal
+            //        {
+            //            if (sinal == "")
+            //            {
+            //                if(sinal == ",")
+            //                    operadorA += ",";
+            //                else
+            //                    sinal = posfixa[i] + "";
+            //            }
+            //            else
+            //            {
+            //               //uee
+            //            }                            
+            //        }               
+            //}
         }
 
-        public string ResolverUmaOperacao(string a, string b,string s)
-        {
-            double? re = null;
+        //public string ResolverUmaOperacao(string a, string b,string s)
+        //{
+            //double? re = null;
 
-            //re = a + s + b;
+            ////re = a + s + b;
 
-            if (s == "+")
-                re = double.Parse(a) + double.Parse(b);
-            else
-            {
-                if(s == "-")
-                    re = double.Parse(a) - double.Parse(b);
-                else
-                {
-                    if(s == "/")
-                        re = double.Parse(a) / double.Parse(b);
-                    else
-                    {
-                        if(s == "*")
-                            re = double.Parse(a) * double.Parse(b);
-                        else
-                        {
-                            if (s == "^")
-                            {
-                                double v = double.Parse(a);
-                                v = Math.Exp(double.Parse(b));
-                                re = v;
-                            }                            
-                        }
-                    }
-                }
-            }
+            //if (s == "+")
+            //    re = double.Parse(a) + double.Parse(b);
+            //else
+            //{
+            //    if(s == "-")
+            //        re = double.Parse(a) - double.Parse(b);
+            //    else
+            //    {
+            //        if(s == "/")
+            //            re = double.Parse(a) / double.Parse(b);
+            //        else
+            //        {
+            //            if(s == "*")
+            //                re = double.Parse(a) * double.Parse(b);
+            //            else
+            //            {
+            //                if (s == "^")
+            //                {
+            //                    double v = double.Parse(a);
+            //                    v = Math.Exp(double.Parse(b));
+            //                    re = v;
+            //                }                            
+            //            }
+            //        }
+            //    }
+            //}
 
-            return re + "";
-        }
+            //return re + "";
+        //}
 
         public bool EstaEmOrdem(PilhaHerdaLista<string> pilha) //Verificar se não tem sinal seguido de sinal e se todos os parênteses foram abertos e fechados igualmente
         {
